1738b0604ea7112e7ed94a98af9d8539f9c8fa6a
Manko Ivan <inoi2002@gmail.com>
2019-12-02 15:13:55 +0300

    signin and signup controllers

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const { errors, celebrate, Joi } = require("celebrate");
 
 const { PORT = 3000 } = process.env;
+
+const { createUser, login } = require("./controllers/users");
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,6 +15,32 @@ mongoose.connect("mongodb://localhost:27017/newsapi-db", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
+});
+
+app.use("/signin", celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+app.use("/signup", celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), createUser);
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? "На сервере произошла ошибка"
+        : message,
+    });
 });
 
 app.listen(PORT, () => {
